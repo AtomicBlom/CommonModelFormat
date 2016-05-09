a28fecffb0fab9f5bda4c872cfd8db1eaa45e3c4
RainWarrior <rainwarrior@fry.su>
2016-05-09 09:40:51 +0300

    Updated old animation code, made it work with non-scalar values.

--- src/main/java/com/github/atomicblom/client/model/cmf/opengex/OpenGEXAnimation.java
+++ src/main/java/com/github/atomicblom/client/model/cmf/opengex/OpenGEXAnimation.java
@@ -53,7 +53,7 @@ class OpenGEXAnimation implements IAnimation
         {
             if(tracks.containsKey(transform))
             {
-                ret = ret.compose(applyTrack(tracks.get(transform), time));
+                ret = ret.compose(getCurrentValueForTrack(tracks.get(transform), time, 1)); // TODO: fps
             }
             else
             {
@@ -63,58 +63,19 @@ class OpenGEXAnimation implements IAnimation
         return ret;
     }
 
-    private TRSRTransformation applyTrack(OgexTrack track, float time)
+    private TRSRTransformation getTrackData(OgexTrack track, float[] value)
     {
-        getCurrentValueForTrack(track, time, 1); //TODO: fps
-
-        if (track.getTime().getCurve() != Curve.Linear || track.getValue().getCurve() != Curve.Linear)
-        {
-            //throw new NotImplementedException("Only linear for now");
-        }
-        float[] times = (float[]) track.getTime().getKeys()[0].getData();
-        if (time == 0)
-        {
-            return getTrackData(track, 0);
-        }
-        int i0 = 0, i1 = times.length - 1;
-        // can't be bothered to write a binsearch, FIXME later
-        // TODO metric
-        while (i0 + 1 < times.length && times[i0 + 1] <= time) i0++;
-        while (i1 - 1 >= 0 && times[i1 - 1] > time) i1--;
-
-        float t0 = times[i0];
-        float t1 = times[i1];
-        float s;
-
-        if (Math.abs(t0 - t1) < 1e-5)
-        {
-            s = t0;
-        }
-        else
-        {
-            s = (time - t0) / (t1 - t0);
-        }
-
-        TRSRTransformation v0 = getTrackData(track, i0);
-        TRSRTransformation v1 = getTrackData(track, i1);
-
-        return v0.slerp(v1, s);
-    }
-
-    private TRSRTransformation getTrackData(OgexTrack track, int keyIndex)
-    {
-        Object v = track.getValue().getKeys()[0].getData();
         Object target = track.getTarget();
         Matrix4f transform = new Matrix4f();
         if (target instanceof OgexMatrixTransform)
         {
             OgexMatrixTransform mt = (OgexMatrixTransform) target;
-            transform.set(((float[][]) v)[keyIndex]);
+            transform.set(value);
             transform.transpose();
         } else if (target instanceof OgexRotation.ComponentRotation)
         {
             OgexRotation.ComponentRotation rot = (OgexRotation.ComponentRotation) target;
-            float angle = ((float[]) v)[keyIndex];
+            float angle = value[0];
             AxisAngle4f aa = new AxisAngle4f(0, 0, 0, angle);
             switch (rot.getKind())
             {
@@ -133,14 +94,14 @@ class OpenGEXAnimation implements IAnimation
             transform.set(aa);
         } else
         {
-            throw new NotImplementedException("Only Matrix Transform for now");
+            throw new NotImplementedException("Only Matrix Transform or simple rotation for now");
         }
         transform.mul(upAxis, transform);
         transform.mul(upAxisInverted);
         return new TRSRTransformation(transform);
     }
 
-    private static float getCurrentValueForTrack(OgexTrack track, float animationTime, float timeScale) {
+    private TRSRTransformation getCurrentValueForTrack(OgexTrack track, float animationTime, float timeScale) {
         final TimeData keyData = getKeyData(track.getTime());
         final ValueData valueData = getValueData(track.getValue());
         int index = getKeyIndexForTime(keyData, timeScale, animationTime);
@@ -152,44 +113,64 @@ class OpenGEXAnimation implements IAnimation
             adjustedTime = getAdjustedLinearTime(keyData, timeScale, animationTime, index);
         }
 
-        float trackValue = 1;
+        TRSRTransformation trackValue = TRSRTransformation.identity();
         if (track.getValue().getCurve() == Curve.Bezier) {
-            trackValue = getAdjustedBezierValue(valueData, index, adjustedTime);
+            trackValue = getAdjustedBezierValue(track, valueData, index, adjustedTime);
         } else if (track.getValue().getCurve() == Curve.Linear) {
-            trackValue = getAdjustedLinearValue(valueData, index, adjustedTime);
+            trackValue = getAdjustedLinearValue(track, valueData, index, adjustedTime);
         }
 
 
         return trackValue;
     }
 
-    private static float getAdjustedLinearValue(ValueData valueData, int index, float adjustedTime) {
-        final float[] value = valueData.value;
+    private TRSRTransformation getAdjustedLinearValue(OgexTrack track, ValueData valueData, int index, float adjustedTime) {
+        final float[][] value = valueData.value;
         final float s = adjustedTime;
-        final float v1 = value[index - 1];
-        final float v2 = value[index];
+        // FIXME: be more careful with indices
+        final TRSRTransformation v1 = getTrackData(track, value[index == 0 ? 0 : index - 1]);
+        final TRSRTransformation v2 = getTrackData(track, value[index]);
 
         //float si = (s - v1) / (v2 - v1);
 
-        return (s * (v2 - v1)) + v1;
+        return v1.slerp(v2, s);
     }
 
-    private static float getAdjustedBezierValue(ValueData valueData, int index, float adjustedTime) {
-        final float[] value = valueData.value;
+    private TRSRTransformation getAdjustedBezierValue(OgexTrack track, ValueData valueData, int index, float adjustedTime) {
+        final float[][] value = valueData.value;
         final float s = adjustedTime;
-        final float v1 = value[index - 1];
-        final float p1 = valueData.positiveControl[index - 1];
-        final float p2 = valueData.negativeControl[index];
-        final float v2 = value[index];
+        // FIXME: be more careful with indices
+        final float[] v1 = value[index == 0 ? 0 : index - 1];
+        final float[] p1 = valueData.positiveControl[index == 0 ? 0 : index - 1];
+        final float[] p2 = valueData.negativeControl[index];
+        final float[] v2 = value[index];
 
-        float v = (((1 - s) * (1 - s) * (1 - s)) * v1) +
-                (3 * s * ((1 - s) * (1 - s)) * p1) +
-                (3 * (s * s) * (1 - s) * p2) +
-                (( s * s * s) * v2);
+        // FIXME: maybe choose the method more carefully
+        if(v1.length == 1)
+        {
+            // scalar argument, do scalar interpolation
+            float v =
+                v1[0] *     (1 - s) * (1 - s) * (1 - s) +
+                p1[0] * 3 *      s  * (1 - s) * (1 - s) +
+                p2[0] * 3 *      s  *      s  * (1 - s) +
+                v2[0] *          s  *      s  *      s;
+            return getTrackData(track, new float[]{v});
+        }
+        // do matrix lerp if not
+        Matrix4f m = getTrackData(track, v1).getMatrix(), t;
+        m.mul((1 - s) * (1 - s) * (1 - s));
+        t = getTrackData(track, p1).getMatrix();
+        t.mul(3 * s * (1 - s) * (1 - s));
+        m.add(t);
+        t = getTrackData(track, p2).getMatrix();
+        t.mul(3 * s * s * (1 - s));
+        m.add(t);
+        t = getTrackData(track, v2).getMatrix();
+        t.mul(s * s * s);
+        m.add(t);
 
-        return v;
+        return new TRSRTransformation(m);
     }
-
 
     private static float getAdjustedLinearTime(TimeData keyData, float scale, float currentTime, int index) {
         final float[] value = keyData.value;
@@ -197,7 +178,8 @@ class OpenGEXAnimation implements IAnimation
 
         float t = currentTime;
 
-        final float t1 = value[index - 1] * scale;
+        // FIXME: be more careful with indices
+        final float t1 = value[index == 0 ? 0 : index - 1] * scale;
         final float t2 = value[index] * scale;
 
         // Returns 0 when (t1 < t2) fails.
@@ -224,8 +206,9 @@ class OpenGEXAnimation implements IAnimation
 
         float t = currentTime;
 
-        final float t1 = value[index - 1] * scale;
-        final float c1 = positiveControl[index - 1] * scale;
+        // FIXME: be more careful with indices
+        final float t1 = value[index == 0 ? 0 : index - 1] * scale;
+        final float c1 = positiveControl[index == 0 ? 0 : index - 1] * scale;
         final float c2 = negativeControl[index] * scale;
         final float t2 = value[index] * scale;
 
@@ -306,6 +289,21 @@ class OpenGEXAnimation implements IAnimation
         return timeData;
     }
 
+    private static float[][] expandArrays(Object data)
+    {
+        if(data instanceof float[][])
+        {
+            return (float[][]) data;
+        }
+        float[] values = (float[]) data;
+        float[][] ret = new float[values.length][];
+        for(int i = 0; i < values.length; i++)
+        {
+            ret[i] = new float[]{values[i]};
+        }
+        return ret;
+    }
+
     private static ValueData getValueData(OgexValue value) {
         final OgexKey[] keys = value.getKeys();
         ValueData timeData = new ValueData(keys);
@@ -313,13 +311,13 @@ class OpenGEXAnimation implements IAnimation
         for (OgexKey key : keys) {
             switch (key.getKind()) {
                 case Value:
-                    timeData.value = (float[]) key.getData();
+                    timeData.value = expandArrays(key.getData());
                     break;
                 case PositiveControl:
-                    timeData.positiveControl = (float[]) key.getData();
+                    timeData.positiveControl = expandArrays(key.getData());
                     break;
                 case NegativeControl:
-                    timeData.negativeControl = (float[]) key.getData();
+                    timeData.negativeControl = expandArrays(key.getData());
                     break;
                 case Bias:
                     timeData.bias = (float[]) key.getData();
@@ -337,9 +335,9 @@ class OpenGEXAnimation implements IAnimation
 
     private static class ValueData {
         OgexKey[] keys;
-        float[] value;
-        float[] positiveControl;
-        float[] negativeControl;
+        float[][] value;
+        float[][] positiveControl;
+        float[][] negativeControl;
         float[] bias;
         float[] continuity;
         float[] tension;
